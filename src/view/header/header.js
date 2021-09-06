aa0c76c95a44c11b3d9d842f30123545ddba3bd8
ashwat <ashwat.acharya09@gmail.com>
2021-09-07 01:30:19 +0530

    deployment

--- src/view/header/header.js
+++ src/view/header/header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header">
       <div className="header__logo">
-        <figure role="logo">
+        <figure>
           <img src={`${process.env.PUBLIC_URL}/aqi-icon.png`} alt="logo" />
           <figcaption>Air Quality Index of Indian Cities</figcaption>
         </figure>
